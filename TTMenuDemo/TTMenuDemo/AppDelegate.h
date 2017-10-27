970e83f1a7101a6f9cba422eba6b7bb2dfec08b9
tennyxu <tennyxu@tencent.com>
2017-10-27 17:20:11 +0800

    Release 1.0.0

---
+++ TTMenuDemo/TTMenuDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTMenuDemo
+//
+//  Created by tennyxu on 27/10/2017.
+//  Copyright © 2017 tennyxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TTMenuDemo/TTMenuDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TTMenuDemo
+//
+//  Created by tennyxu on 27/10/2017.
+//  Copyright © 2017 tennyxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
